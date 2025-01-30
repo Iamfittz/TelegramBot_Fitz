a8d35b13fcf8420db59f380dd8b09c9bb08a7490
Iamfitz <vuitsyk.a@gmail.com>
2025-01-30 13:04:52 +0200

    Update States for OIS needs

--- Bot/UserState.cs
+++ Bot/UserState.cs
@@ -4,7 +4,8 @@
     {
         None,
         FixedRate,
-        FloatingRate
+        FloatingRate,
+        OIS
     }
 
     public class UserState
@@ -12,6 +13,7 @@
         public int Step { get; set; } = 0;  
         public decimal LoanAmount { get; set; }
         public int LoanYears { get; set; }
+        public int Days {  get; set; }
         public decimal FirstRate { get; set; }   
         public decimal SecondRate { get; set; }
         public CalculationType CalculationType { get; set; } = CalculationType.None; // Тип расчета (Fixed или Floating)
@@ -21,9 +23,11 @@
             Step = 0;
             LoanAmount = 0;
             LoanYears = 0;
+            Days = 0;
             FirstRate = 0;
             SecondRate = 0;
             CalculationType = CalculationType.None; // сбрасываем тип расчета
+            
         }
     }
 }
