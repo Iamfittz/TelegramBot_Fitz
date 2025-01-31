d84266a8d5a4d242d00717ffb60a6c798b7140b3
Iamfitz <vuitsyk.a@gmail.com>
2025-01-31 10:26:03 +0200

    refactor: Update FixedCalculator to support Simple and Compund interest calculation

--- Core/FixedCalculator.cs
+++ Core/FixedCalculator.cs
@@ -7,41 +7,88 @@ namespace TelegramBot_Fitz.Core
     {
         public decimal TotalInterest { get; set; }
         public decimal TotalPayment { get; set; }
+
+        public YearlyCalculation[] YearlyCalculations { get; set; }
     }
 
-    public class FixedRateLoanCalculator
+    public class YearlyCalculation
     {
-        public decimal CalculateInterest(decimal amount, decimal[] yearlyRates)
+        public int Year { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Interest { get; set; }
+        public decimal AccumulatedAmount { get; set; }
+    }
+
+        public class FixedRateLoanCalculator
+    {
+        public decimal CalculateInterest(decimal amount, decimal[] yearlyRates, InterestCalculationType calculationType)
         {
             decimal totalInterest = 0;
+            decimal currentAmount = amount;
+
             for(int i = 0; i < yearlyRates.Length; i++)
             {
-                decimal yearlyInterest = amount * (yearlyRates[i] / 100);
-                totalInterest+= yearlyInterest;
+                decimal yearlyInterest;
+
+                if(calculationType == InterestCalculationType.Simple)
+                {
+                    yearlyInterest = amount * (yearlyRates[i] / 100);
+                    totalInterest += yearlyInterest;
+                }
+                else
+                {
+                    yearlyInterest = currentAmount * (yearlyRates[i] / 100);
+                    currentAmount += yearlyInterest;
+                    totalInterest += yearlyInterest;
+                }
             }
             return totalInterest;
         }
 
-        public LoanCalculationResult CalculateLoan(decimal loanAmount, decimal[] yearlyRates)
+        public LoanCalculationResult CalculateLoan(decimal loanAmount, decimal[] yearlyRates, InterestCalculationType calculationType)
         {
-            var totalInterest = CalculateInterest(loanAmount, yearlyRates);
-            var totalPayment = loanAmount + totalInterest;
+            var yearlyCalculations = new YearlyCalculation[yearlyRates.Length];
+            decimal totalInterest = 0;
+            decimal currentAmount = loanAmount;
+
+            // Расчет для каждого года
+            for (int i = 0; i < yearlyRates.Length; i++)
+            {
+                decimal yearlyInterest;
+                if (calculationType == InterestCalculationType.Simple)
+                {
+                    yearlyInterest = loanAmount * (yearlyRates[i] / 100);
+                    yearlyCalculations[i] = new YearlyCalculation
+                    {
+                        Year = i + 1,
+                        Rate = yearlyRates[i],
+                        Interest = yearlyInterest,
+                        AccumulatedAmount = loanAmount + yearlyInterest
+                    };
+                }
+                else // Compound
+                {
+                    yearlyInterest = currentAmount * (yearlyRates[i] / 100);
+                    currentAmount += yearlyInterest;
+                    yearlyCalculations[i] = new YearlyCalculation
+                    {
+                        Year = i + 1,
+                        Rate = yearlyRates[i],
+                        Interest = yearlyInterest,
+                        AccumulatedAmount = currentAmount
+                    };
+                }
+                totalInterest += yearlyInterest;
+            }
 
             return new LoanCalculationResult
             {
                 TotalInterest = totalInterest,
-                TotalPayment = totalPayment
+                TotalPayment = loanAmount + totalInterest,
+                YearlyCalculations = yearlyCalculations
             };
         }
 
-        public string FormatCalculationResult(LoanCalculationResult result, decimal[] yearlyRates)
-        {
-            var ratesByYear = string.Join("\n", yearlyRates.Select((rate, index) =>
-            $"Year {index + 1} rate: {rate}%"));
-            
-            return $"Rates by year:\n{ratesByYear}\n\n"+
-                   $"The total interest is:{result.TotalInterest:F2} USD.\n" +
-                   $"The total payment is: {result.TotalPayment:F2} USD.";
-        }
+
     }
 }
