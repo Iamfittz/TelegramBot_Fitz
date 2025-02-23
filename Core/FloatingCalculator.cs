ad7ecb0ea24b1a92051d2cb47807876ece30585b
Iamfitz <vuitsyk.a@gmail.com>
2025-02-23 18:51:03 +0200

    Refactored FloatingRateLoanCalculator to integrate with Strategy Pattern for interest calculations.

--- Core/FloatingCalculator.cs
+++ Core/FloatingCalculator.cs
@@ -3,32 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TelegramBot_Fitz.Bot;
 
 namespace TelegramBot_Fitz.Core
 {
-    public class FloatingRateLoanCalculator
+    public class FloatingRateLoanCalculator : ILoanCalculator
     {
         public decimal LoanAmount { get; set; }
         public int TotalYears {  get; set; }
         public decimal FirstRate {  get; set; }
         public decimal SecondRate { get; set; }
-        public decimal CalculateFirstPeriodInterest()
+       
+        public decimal CalculateInterest(UserState state)
         {
-            return LoanAmount*(FirstRate/100)*(6/12m);
+            return CalculateFirstPeriodInterest(state) + CalculateSecondPeriodInterest(state);
         }
-        public decimal CalculateSecondPeriodInterest()
+        public decimal CalculateFirstPeriodInterest(UserState state)
+        {
+            return LoanAmount * (FirstRate / 100) * (6 / 12m);
+        }
+        public decimal CalculateSecondPeriodInterest(UserState state)
         {
             return LoanAmount * (SecondRate / 100) * (6 / 12m);
         }
 
-        public decimal CalculateTotalInterest()
+        public decimal CalculateTotalInterest(UserState state)
         {
-            return CalculateFirstPeriodInterest() + CalculateSecondPeriodInterest();
+            return CalculateFirstPeriodInterest(state) + CalculateSecondPeriodInterest(state);
         }
 
-        public decimal CalculateTotalPayment()
+        public decimal CalculateTotalPayment(UserState state)
         {
-            return LoanAmount + CalculateTotalInterest();
+            return LoanAmount + CalculateTotalInterest(state);
         }
     }
 }
